464dd0e640c86304db6feb075ccde0a5e09eb060
Jordy Michael Ward <24913707@sun.ac.za>
2024-11-01 20:15:27 +0200

    managed to fix vs code extensions and error highlighting I think. Also managed to learn how to instantiate a map of letters and values in Scrabble.java class

--- demo/src/main/java/com/project1/Scrabble.java
+++ demo/src/main/java/com/project1/Scrabble.java
@@ -1,44 +1,20 @@
 package com.project1;
 
-protected class Scrabble {
+import java.util.Map;
+import static java.util.Map.entry;
+
+public class Scrabble {
 
     //map that stores the key (letter) value pairs of the game
-    Map<Character, Integer> lettersValues = Map.of(
-        'a',1,
-        'e',2,
-        'i',1,
-        'o',1,
-        'u',1,
-        'l',1,
-        'n',1,
-        'r',1,
-        's',1,
-        't',1,
-
-        'd',2,
-        'g',2,
-
-        'b',3,
-        'c',3,
-        'm',3,
-        'p',3,
-
-        'f',4,
-        'h',4,
-        'v',4,
-        'w',4,
-        'y',4,
-
-        'k',5,
-
-        'j',8,
-        'x',8,
-
-        'q',10,
-        'z',10
-
-
-    )
+    Map<Character, Integer> lettersValues = Map.ofEntries(
+    entry('A', 1), entry('E', 1), entry('I', 1), entry('O', 1), entry('U', 1), entry('L', 1), entry('N', 1), entry('R', 1), entry('S', 1), entry('T', 1),
+    entry('D', 2), entry('G', 2),
+    entry('B', 3), entry('C', 3), entry('M', 3), entry('P', 3),
+    entry('F', 4), entry('H', 4), entry('V', 4), entry('W', 4), entry('Y', 4),
+    entry('K', 5),
+    entry('J', 8), entry('X', 8),
+    entry('Q', 10), entry('Z', 10)
+);
 
      Scrabble() {
     
